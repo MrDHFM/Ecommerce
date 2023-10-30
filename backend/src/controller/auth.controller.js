49ce6419624f4ae4058434d9e6e1cb41f33a42fa
MrDHMF <butchiraju63@gmail.com>
2023-10-31 00:17:11 +0530

    login

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,43 +1,41 @@
-const userService = require("../services/user.service.js")
-const jwtProvider = require("../config/jwtProvider.js")
-const cartService = require("../services/cart.service.js")
-const bcrypt = require("bcrypt")
+const userService = require("../services/user.service.js");
+const jwtProvider = require("../config/jwtProvider.js");
+const cartService = require("../services/cart.service.js");
+const bcrypt = require("bcrypt");
 
-const signUp = async (req,res) => {
-    try {
+const signUp = async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
 
-        const user = await userService.createUser(req.body)
+    await cartService.createCart(user);
 
-        await cartService.createCart(user)
+    return res.status(200).send({ message: "User created successfully" });
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
 
-        return res.status(200).send({message:"User created successfully"})
-        
-    } catch (error) {
-        return res.status(500).send({error:error.message})
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userService.getUserByEmail(email);
+
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: "User not found with this email ", email });
     }
-}
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-const login = async (req,res) => {
-    try {
-
-        const {email,password} = req.body
-
-        const user = await userService.getUserByEmail(email)
-
-        if(!user){
-            return res.status(404).send({message:"User not found with email :",email})
-        }
-        const isPasswordValid = await bcrypt.compare(password,user.password)
-
-if(!isPasswordValid){
-    res.status(401).send({message:"Invalid Password"})
-}
-const jwt = jwtProvider.generateToken(user._id)
-return res.status(200).send({jwt,message:"Login Success"})
-
-    } catch (error) {
-        return res.status(500).send({error:error.message})
+    if (!isPasswordValid) {
+      return res.status(401).send({ message: "Invalid Password" });
     }
-}
+    const jwt = jwtProvider.generateToken(user._id);
+    return res.status(200).send({ jwt, message: "Login Success" });
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
 
-module.exports = {signUp,login}
+module.exports = { signUp, login };
